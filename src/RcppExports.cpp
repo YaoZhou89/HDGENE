8993a9dc8d4174a81f736963f53fb10e2126cd42
GST <zhouyao1989@163.com>
2018-02-07 03:39:01 +0800

    New features removed

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -21,9 +21,27 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// geno_cor_new2
+RcppExport SEXP geno_cor_new2(SEXP YY, SEXP GDD, SEXP GDDD, SEXP ww, SEXP coorr, SEXP mm, SEXP KK);
+RcppExport SEXP _HDGENE_geno_cor_new2(SEXP YYSEXP, SEXP GDDSEXP, SEXP GDDDSEXP, SEXP wwSEXP, SEXP coorrSEXP, SEXP mmSEXP, SEXP KKSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< SEXP >::type YY(YYSEXP);
+    Rcpp::traits::input_parameter< SEXP >::type GDD(GDDSEXP);
+    Rcpp::traits::input_parameter< SEXP >::type GDDD(GDDDSEXP);
+    Rcpp::traits::input_parameter< SEXP >::type ww(wwSEXP);
+    Rcpp::traits::input_parameter< SEXP >::type coorr(coorrSEXP);
+    Rcpp::traits::input_parameter< SEXP >::type mm(mmSEXP);
+    Rcpp::traits::input_parameter< SEXP >::type KK(KKSEXP);
+    rcpp_result_gen = Rcpp::wrap(geno_cor_new2(YY, GDD, GDDD, ww, coorr, mm, KK));
+    return rcpp_result_gen;
+END_RCPP
+}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_HDGENE_geno_cor_new", (DL_FUNC) &_HDGENE_geno_cor_new, 6},
+    {"_HDGENE_geno_cor_new2", (DL_FUNC) &_HDGENE_geno_cor_new2, 7},
     {NULL, NULL, 0}
 };
 
--- src/geno_cor_new.cpp
+++ src/geno_cor_new.cpp
@@ -37,98 +37,98 @@ RcppExport SEXP  geno_cor_new ( SEXP  YY,SEXP GDD,SEXP ww,SEXP coorr,SEXP mm,SEX
   return wrap(corr);
 }
 
-NumericVector product(NumericMatrix X, NumericVector Y, int n,int P){
-  // X %*% Y
-  //X: n*p; Y: p*1; prod: n*1
-  NumericVector prod(n);
-  for(int p = 0; p < n; p++){
-    for(int k = 0; k < P; k++){
-      prod(p) += X(p,k)*Y(k) ;
-    }
-  }
-  return prod;
-}
-
-NumericMatrix Mproduct(NumericMatrix X, NumericMatrix Y, int a,int b1,int b2,int c ){
-  // X * Y
-  NumericMatrix prod(a,c);
-  for(int p = 0; p < a; p++){
-    for(int k = 0; k < c; k++){
-      for( int t = 0; t < b1; t++){
-        prod(p,k) += X(p,t)*Y(t,k) ;
-      }
-    }
-  }
-  return prod;
-}
-
-NumericMatrix Mcrossproduct(NumericMatrix X, NumericMatrix Y, int a,int b,int a2,int c ){
-  // T(X) * Y
-  NumericMatrix prod(b,c);
-  for(int p = 0; p < b; p++){
-    for(int k = 0; k < c; k++){
-      for( int t = 0; t < a; t++){
-        prod(p,k) += X(t,p)*Y(t,k) ;
-      }
-    }
-  }
-  return prod;
-}
-
-NumericVector crossprod(NumericMatrix X, NumericVector Y, int P,int K){
-  // t(X) %*% Y
-  //X: p*n; Y: p*1, prod: p*1
-  NumericVector prod(P);
-  for(int p = 0; p < P; p++){
-    for(int k = 0; k < K; k++){
-      prod(p) += X(k,p)*Y(k) ;
-    }
-  }
-  return prod;
-}
-
-NumericVector getInteract(NumericVector X, NumericVector Y,int m){
-  NumericVector prod(m);
-  for(int p = 0; p < m; p++){
-      prod(p) = X(p)*Y(p) ;
-  }
-  return prod;
-}
-double colSumsq(NumericVector X, int n){
-  double sum = 0;
-  for ( int i = 0; i < n; i++){
-    sum += X(i)*X(i);
-  }
-  return sum;
-}
-double getAbs (NumericVector X, NumericVector Y,int n){
-  double r = 0;
-  for (int i = 0; i < n; i++){
-    r += X(i)*Y(i);
-  }
-  if (r < 0) r = -r;
-  return r;
-}
-// NumericVector getMinus(NumericVector X, )
-// inline static double sqrt_double( double x ){ return ::sqrt( x ); }
-// [[Rcpp::export]]
-NumericVector  geno_cor (NumericVector & Y, NumericMatrix & GD, NumericMatrix & w,int m, int n,int p) {
-  NumericMatrix corr(m,m);
-  NumericVector GDi(n);
-  NumericVector GDt(n),GDtt(n),GDttt(n);
-  double div,r,colsq;
-  for (int i = 0 ; i < m; i++){
-    for(int j = 0; j < i + 1; j++){
-      r = 0;
-      GDi = getInteract(GD(i,_),GD(j,_),n);
-      GDt = product(w,crossprod(w,GDi,p,n),n,p);
-      GDtt = GDi - GDt;
-      colsq = colSumsq(GDtt,n);
-      div = sqrt(colsq);
-      GDttt = GDtt/div;
-      r = getAbs(GDttt,Y,n);
-      corr(i,j) = r;
-    }
-  }
-  return corr;
-}
+// NumericVector product(NumericMatrix X, NumericVector Y, int n,int P){
+//   // X %*% Y
+//   //X: n*p; Y: p*1; prod: n*1
+//   NumericVector prod(n);
+//   for(int p = 0; p < n; p++){
+//     for(int k = 0; k < P; k++){
+//       prod(p) += X(p,k)*Y(k) ;
+//     }
+//   }
+//   return prod;
+// }
+//
+// NumericMatrix Mproduct(NumericMatrix X, NumericMatrix Y, int a,int b1,int b2,int c ){
+//   // X * Y
+//   NumericMatrix prod(a,c);
+//   for(int p = 0; p < a; p++){
+//     for(int k = 0; k < c; k++){
+//       for( int t = 0; t < b1; t++){
+//         prod(p,k) += X(p,t)*Y(t,k) ;
+//       }
+//     }
+//   }
+//   return prod;
+// }
+//
+// NumericMatrix Mcrossproduct(NumericMatrix X, NumericMatrix Y, int a,int b,int a2,int c ){
+//   // T(X) * Y
+//   NumericMatrix prod(b,c);
+//   for(int p = 0; p < b; p++){
+//     for(int k = 0; k < c; k++){
+//       for( int t = 0; t < a; t++){
+//         prod(p,k) += X(t,p)*Y(t,k) ;
+//       }
+//     }
+//   }
+//   return prod;
+// }
+//
+// NumericVector crossprod(NumericMatrix X, NumericVector Y, int P,int K){
+//   // t(X) %*% Y
+//   //X: p*n; Y: p*1, prod: p*1
+//   NumericVector prod(P);
+//   for(int p = 0; p < P; p++){
+//     for(int k = 0; k < K; k++){
+//       prod(p) += X(k,p)*Y(k) ;
+//     }
+//   }
+//   return prod;
+// }
+//
+// NumericVector getInteract(NumericVector X, NumericVector Y,int m){
+//   NumericVector prod(m);
+//   for(int p = 0; p < m; p++){
+//       prod(p) = X(p)*Y(p) ;
+//   }
+//   return prod;
+// }
+// double colSumsq(NumericVector X, int n){
+//   double sum = 0;
+//   for ( int i = 0; i < n; i++){
+//     sum += X(i)*X(i);
+//   }
+//   return sum;
+// }
+// double getAbs (NumericVector X, NumericVector Y,int n){
+//   double r = 0;
+//   for (int i = 0; i < n; i++){
+//     r += X(i)*Y(i);
+//   }
+//   if (r < 0) r = -r;
+//   return r;
+// }
+// // NumericVector getMinus(NumericVector X, )
+// // inline static double sqrt_double( double x ){ return ::sqrt( x ); }
+// // [[Rcpp::export]]
+// NumericVector  geno_cor (NumericVector & Y, NumericMatrix & GD, NumericMatrix & w,int m, int n,int p) {
+//   NumericMatrix corr(m,m);
+//   NumericVector GDi(n);
+//   NumericVector GDt(n),GDtt(n),GDttt(n);
+//   double div,r,colsq;
+//   for (int i = 0 ; i < m; i++){
+//     for(int j = 0; j < i + 1; j++){
+//       r = 0;
+//       GDi = getInteract(GD(i,_),GD(j,_),n);
+//       GDt = product(w,crossprod(w,GDi,p,n),n,p);
+//       GDtt = GDi - GDt;
+//       colsq = colSumsq(GDtt,n);
+//       div = sqrt(colsq);
+//       GDttt = GDtt/div;
+//       r = getAbs(GDttt,Y,n);
+//       corr(i,j) = r;
+//     }
+//   }
+//   return corr;
+// }
